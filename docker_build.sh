200d09ecb0f1615d8c136f5d9f59dba902036da4
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-04-20 12:08:06 -0700

    Recover the right files from the container.

--- docker_build.sh
+++ docker_build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 set -e
+set -x
 
 docker build --build-arg release=$release --build-arg version=$version --build-arg triple=$triple --build-arg threads=${threads:-4} -t terralang/llvm-build:$distro-$release-llvm$version -f Dockerfile.$distro .
 
 # Copy files out of container.
 tmp=$(docker create terralang/terra:$distro-$release-llvm$version)
-docker cp $tmp:/llvm .
+docker cp $tmp:/llvm/*.tar.xz .
 docker rm $tmp
