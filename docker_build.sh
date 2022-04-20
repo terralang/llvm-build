2f248b6f8816ff6ca3127504fd00fe68c970c4bd
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-04-20 13:33:00 -0700

    Fix a typo in Docker build.

--- docker_build.sh
+++ docker_build.sh
@@ -6,6 +6,6 @@ set -x
 docker build --build-arg release=$release --build-arg version=$version --build-arg triple=$triple --build-arg threads=${threads:-4} -t terralang/llvm-build:$distro-$release-llvm$version -f Dockerfile.$distro .
 
 # Copy files out of container.
-tmp=$(docker create terralang/terra:$distro-$release-llvm$version)
+tmp=$(docker create terralang/llvm-build:$distro-$release-llvm$version)
 docker cp $tmp:/llvm/*.tar.xz .
 docker rm $tmp
